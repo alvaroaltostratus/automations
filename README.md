b8e0c97a0dccdcbf712348d29d03b36a265e0772
Altostratus <altostratus@Altostratuss-MacBook-Pro.local>
2023-01-31 10:38:36 +0100

    [ADD] Add antivirus automation (in-progress)

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# automations
-Automatizaciones del apartado GCP Administrado del Manual de Provisión
+# Automations
+
+## Antivirus (en proceso)
+
+### Requerimientos
+1. Terraform (https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/install-cli)
+2. Terraformer (https://github.com/GoogleCloudPlatform/terraformer)
+3. Python (https://www.python.org/downloads/)
+---
+### Ejecución
+1. Ve a la carpeta "antivirus/terraform-new"
+2. Cambia el archivo `terraform.tfvars` y cambia el proyecto y la región
+3. Ejecuta `terraform init` y `terraform apply --auto-approve`
+ --
+4. Ve a la carpeta "antivirus/terraform-import"
+5. Ejecuta `terraformer import google -r instances -z <región> --projects <proyecto> -p "{service}"-C -O json`
+--
+6. Ve a la carpeta "scripts"
+7. Ejecuta `python antivirus.py`
+---
+
+## Backup de instancias de Compute Engine
+
+### Requerimientos
+1. Terraform (https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/install-cli)
+2. Python (https://www.python.org/downloads/)
+---
+### Ejecución
+1. Ve a la carpeta "backup-vm-instances/terraform"
+2. Cambia el archivo `terraform.tfvars` y cambia el proyecto y la región
+--
+3. Ve a la carpeta "backup-vm-instances"
+4. Ejecuta `./execute.sh`
+---
---
+++ antivirus/scripts/antivirus.py
@@ -0,0 +1,32 @@
+import json
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+if __name__ == '__main__':
+
+    with open('../terraform-import/instances/resources.tf.json') as f:
+        instances = json.loads(f.read())["resource"]["google_compute_instance"]
+        credentials = GoogleCredentials.get_application_default()
+        service = discovery.build('compute', 'v1', credentials=credentials)
+
+        for k, v in instances.items():
+            curr_metadata = service.instances().get(project=v["project"], zone=v["zone"], instance=v["name"]).execute()["metadata"]
+
+            if "items" in curr_metadata:
+                curr_metadata["items"].append({
+                    "key": 'startup-script',
+                    "value": "hola"
+                })
+            else:
+                curr_metadata["items"] = [{
+                    "key":'startup-script',
+                    "value": "hola"
+                }]
+            
+            try:
+                response = service.instances().setMetadata(project=v["project"], zone=v["zone"], instance=v["name"], body=curr_metadata).execute()
+                print(f"Applied startup-script to {v['name']}")
+            except Exception as e:
+                print(f"Failed to apply startup-script to {v['name']}")
+            
