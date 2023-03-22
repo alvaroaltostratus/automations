6f7f3e2195d9602e14812d7f180c31f0bbb4c585
Álvaro Rodríguez <alvaromarianorodriguezbenitez@gmail.com>
2023-03-22 14:03:00 +0100

    [ADD] Logs cuando aplicas política de backup

--- backup-vm-instances/assign-snapshot-policy-to-disks.sh
+++ backup-vm-instances/assign-snapshot-policy-to-disks.sh
@@ -11,6 +11,12 @@ IFS=','
 
 for disk in $disks; do
     gcloud compute disks add-resource-policies "$disk" --resource-policies="$3" --project="$1" --zone="$2"
+
+    if [ $? -eq 0 ]; then
+        echo "Agregada política de backup (`gcloud compute disks describe \"$disk\" --zone=\"$2\" --project=\"$1\" --format=\"value(resourcePolicies)\"`) al disco $disk"
+    else
+        echo "Ha habido un error agregando la política de backup al disco $disk."
+    fi
 done
 
 unset IFS
