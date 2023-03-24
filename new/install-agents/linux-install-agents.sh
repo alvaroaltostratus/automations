632ec5e52a8388436327dd1b17c211b297fc2205
Álvaro Rodríguez <alvaromarianorodriguezbenitez@gmail.com>
2023-03-24 11:59:31 +0100

    [ADD] Restart script

--- new/install-agents/linux-install-agents.sh
+++ new/install-agents/linux-install-agents.sh
@@ -9,3 +9,38 @@ sudo apt update
 sudo apt -y install google-osconfig-agent
 
 # Instalar antivirus
+
+
+# Configurar Apache2
+set -e
+
+# Create a back up of the existing file so existing configurations are not lost.
+sudo cp /etc/google-cloud-ops-agent/config.yaml /etc/google-cloud-ops-agent/config.yaml.bak
+
+# Configure the Ops Agent.
+sudo tee /etc/google-cloud-ops-agent/config.yaml > /dev/null << EOF
+metrics:
+  receivers:
+    apache:
+      type: apache
+  service:
+    pipelines:
+      apache:
+        receivers:
+          - apache
+logging:
+  receivers:
+    apache_access:
+      type: apache_access
+    apache_error:
+      type: apache_error
+  service:
+    pipelines:
+      apache:
+        receivers:
+          - apache_access
+          - apache_error
+EOF
+
+sudo service google-cloud-ops-agent restart
+sleep 60
--- new/install-agents/windows-install-agents.ps1
+++ new/install-agents/windows-install-agents.ps1
@@ -4,3 +4,39 @@ Invoke-Expression "${env:UserProfile}\add-google-cloud-ops-agent-repo.ps1 -AlsoI
 
 # Instalar OS-CONFIG-AGENT
 googet -noconfirm install google-osconfig-agent
+
+Start-Sleep -s 5
+
+# Instalar Antivirus
+
+
+# Configurar IIS
+Copy-Item -Path 'C:\Program Files\Google\Cloud Operations\Ops Agent\config\config.yaml' -Destination 'C:\Program Files\Google\Cloud Operations\Ops Agent\config\config.yaml.bak'
+
+# Configure the Ops Agent.
+Add-Content 'C:\Program Files\Google\Cloud Operations\Ops Agent\config\config.yaml' "
+metrics:
+  receivers:
+    iis_v2:
+      type: iis
+      receiver_version: 2
+  service:
+    pipelines:
+      iispipeline:
+        receivers:
+          - iis_v2
+logging:
+  receivers:
+    iis_access:
+      type: iis_access
+  service:
+    pipelines:
+      iis:
+        receivers:
+        - iis_access
+"
+
+Start-Sleep -s 5
+
+Stop-Service google-cloud-ops-agent -Force
+Start-Service google-cloud-ops-agent* 
